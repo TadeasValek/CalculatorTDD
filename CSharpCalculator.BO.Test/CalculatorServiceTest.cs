a4e933749c7df1a34f32d6c0e2e1f4eb005a09a9
valek.tadeas@email.cz <valek.tadeas@email.cz>
2017-05-19 12:16:01 +0200

    check in

---
+++ CSharpCalculator.BO.Test/CalculatorServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CSharpCalculator.BO;
+
+namespace CSharpCalculator.BO.Test
+{
+    [TestFixture]
+    public class CalculatorServiceTest
+    {
+        [TestCase("1", "5", "15")]
+        [TestCase("2", "5", "25")]
+        [TestCase("0", "5", "5")]
+        [TestCase("1", "a", "5")] //expected is previous input
+        [TestCase("a", "5", "0")] //expected is 0
+
+        public void ParseInputTest(string previousInput, string currentInput, string expectedResult)
+        {
+
+            string actualResult = CalculatorService.ParseInput(previousInput, currentInput);
+            Assert.That(actualResult, Is.EqualTo(expectedResult));            
+            //string Result = "WrongInput123";
+
+            //int intResult;
+            //bool isResultInt = int.TryParse(Result,out intResult);
+            //Assert.That(isResultInt, Is.Not.False);
+
+        }
+    }
+}
---
+++ CSharpCalculator.BO/CalculatorService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCalculator.BO
+{
+    public static class CalculatorService
+    {
+        public static String ParseInput(string previousInput, string currentInput)
+        {
+            //return int.Parse(previousInput + currentInput).ToString();
+            return Convert.ToString(int.Parse(previousInput + currentInput));
+        }
+
+    }
+}
